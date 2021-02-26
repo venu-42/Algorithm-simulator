95f0d91a1f7057cf161ddeb7bac05a643ff19b10
venu-42 <solipuram42@gmail.com>
2021-02-26 12:06:55 +0530

    added bfs functionality & removed btns

---
+++ js/bfs.js
@@ -0,0 +1,83 @@
+function bfs(){
+    let queue=[];
+        queue.push([src_x-1,src_y-1]);
+
+        let visited=[],parent=[];               
+        for(let i=0;i<height;i++){
+            let temp=[],temp1=[];
+            for(let j=0;j<width;j++){
+                temp.push(0);
+                temp1.push([0,0]);
+            }
+            visited.push(temp);
+            parent.push(temp1);
+        }
+
+    //util fn
+    function push_addclass(x,y,p_x,p_y){
+        // setTimeout(()=>{
+            visited[x][y]=1;
+            if(virtual_grid[x][y]==1) return ;
+            parent[x][y]=[p_x,p_y];
+            queue.push([x,y]);
+            // console.log('pushed');
+            
+        // },0);
+    }
+
+    function findPath(){
+        let arr=[];
+        let x=dest_x-1,y=dest_y-1;
+        console.log('inside finpath',x,y);
+        arr.unshift([x,y]);
+        for(;!(x==src_x-1&&y==src_y-1);){
+            [x,y]=[...parent[x][y]]
+            console.log(x,y);
+            arr.unshift([x,y]);
+        }
+        return arr;
+    }
+
+    function utilfunc(i,path){
+        setTimeout(function(){
+            console.log(`${path[i][0]+1}-${path[i][1]+1}`);
+            document.getElementById(`${path[i][0]+1}-${path[i][1]+1}`).classList.add('pathed');
+        },150*i);
+    }
+    function createPath(path){
+        for(let i=0;i<path.length;i++){
+            utilfunc(i,path);
+        }
+    }
+
+    visited[src_x-1][src_y-1]=1;
+    // for(;queue.length>0;){
+    let timer=setInterval(()=>{
+        console.log('ran');
+        if(queue.length==0) {
+            clearInterval(timer);
+            return;
+        }
+        let [tx,ty]=queue.shift();
+        document.getElementById(`${tx+1}-${ty+1}`).classList.add('visited');
+        if(tx+1==dest_x&&ty+1==dest_y){
+            console.log('finally found!!!');
+            clearInterval(timer);
+            let path=findPath();
+            createPath(path);
+        }
+        // if(tx-1>=0&&ty-1>=0&&!visited[tx-1][ty-1]) push_addclass(tx-1,ty-1,tx,ty);
+        if(tx-1>=0&&ty>=0&&!visited[tx-1][ty]) push_addclass(tx-1,ty,tx,ty);
+        // if(tx-1>=0&&ty+1<width&&!visited[tx-1][ty+1]) push_addclass(tx-1,ty+1,tx,ty);
+        if(tx>=0&&ty+1<width&&!visited[tx][ty+1]) push_addclass(tx,ty+1,tx,ty);
+        // if(tx+1<height&&ty+1<width&&!visited[tx+1][ty+1]) push_addclass(tx+1,ty+1,tx,ty);
+        if(tx+1<height&&ty>=0&&!visited[tx+1][ty]) push_addclass(tx+1,ty,tx,ty);
+        // if(tx+1<height&&ty-1>=0&&!visited[tx+1][ty-1]) push_addclass(tx+1,ty-1,tx,ty);
+        if(tx>=0&&ty-1>=0&&!visited[tx][ty-1]) push_addclass(tx,ty-1,tx,ty);
+    },10);
+    console.log('exited');
+}
+
+document.querySelector('#bfs-btn').addEventListener('click',function(){
+    bfs();
+})
--- js/grid.js
+++ js/grid.js
@@ -1,26 +1,31 @@
-let controlState='select_boxes';
-let isMouseDown=0;
-let [src_x,src_y]=[5,10];
-let [dest_x,dest_y]=[9,17];
+// let controlState='select_boxes';
+let isMouseDown=0,mouseDown_src=0,isMouseDownForMovingIcons=0;
+
+let height=25,width=40;
+
+let [src_x,src_y]=[1,1];
+let [dest_x,dest_y]=[1,1];
 let virtual_grid=[];
 
 
 let grid_table=document.querySelector('.grid-table');
 let icon_src=document.createElement('i');
 let icon_dest=document.createElement('i');
+icon_src.setAttribute('class','bi bi-pin-fill');
+icon_dest.setAttribute('class','bi bi-person-fill');
 
 // setting up the grid and src,dest points
 
-for(let i=0;i<25;i++){
+for(let i=0;i<height;i++){
     let temp=[]
-    for(let j=0;j<40;j++){
+    for(let j=0;j<width;j++){
         temp.push(0);
     }
     virtual_grid.push(temp);
 }
-for(let i=0;i<25;i++){
+for(let i=0;i<height;i++){
     let row=document.createElement('tr');
-    for(let j=0;j<40;j++){
+    for(let j=0;j<width;j++){
         let box=document.createElement('td');
         box.setAttribute('id',`${i+1}-${j+1}`);
         box.setAttribute('class','box');
@@ -31,13 +36,11 @@ for(let i=0;i<25;i++){
 
 function set_src(x,y){
     [src_x,src_y]=[x,y];
-    icon_src.setAttribute('class','bi bi-pin');
     document.getElementById(`${x}-${y}`).appendChild(icon_src);
     document.getElementById(`${x}-${y}`).classList.add('text-center');
 }
 function set_dest(x,y){
     [dest_x,dest_y]=[x,y];
-    icon_dest.setAttribute('class','bi bi-person-fill');
     document.getElementById(`${x}-${y}`).appendChild(icon_dest);
     document.getElementById(`${x}-${y}`).classList.add('text-center');
 }
@@ -46,33 +49,17 @@ function addingEventListeners(){
     console.log('entered addingevents');
     let allboxes=Array.from(document.querySelectorAll('.box'));
 
-    document.querySelector('#change_src').addEventListener('click',function(){
-        controlState=controlState=="change_src"?"select_boxes":"change_src";
-        if(this.classList.contains('active')) {
-            this.classList.remove('active');
-        }
-        else {
-            this.classList.add('active');
-            document.querySelector('#change_dest').classList.remove('active');
-        }
-    })
-    document.querySelector('#change_dest').addEventListener('click',function(){
-        controlState=controlState=="change_dest"?"select_boxes":"change_dest";
-        if(this.classList.contains('active')) {
-            this.classList.remove('active');
-        }
-        else {
-            this.classList.add('active');
-            document.querySelector('#change_src').classList.remove('active');
-        }
-    })
-    // console.log(allboxes);
 
-    // selection of boxes events
+
+    // ----------------selection of boxes events(mouse down)---------------------
     allboxes.map(box=>box.addEventListener('mousedown',function(event){
         let [x,y]=this.id.split('-');
 
-        if(controlState!=='select_boxes'||(x==src_x&&y==src_y)||(x==dest_x&&y==dest_y)) return ;
+        if((x==src_x&&y==src_y)||(x==dest_x&&y==dest_y)){
+            isMouseDownForMovingIcons=1;
+            mouseDown_src=(x==src_x&&y==src_y)?1:0;
+            return;
+        }
 
         if(this.classList.contains('checked')) {
             this.classList.remove('checked');
@@ -84,12 +71,21 @@ function addingEventListeners(){
         }
         console.log('cliked');
     }))
-
+    
+    // ----------------selection of boxes events(mouse over)---------------------
     allboxes.map(box=>{
-            box.addEventListener('mouseover',function(){
+            box.addEventListener('mouseover',function(event){
                 let [x,y]=this.id.split('-');
-
-                if(isMouseDown==0||(controlState!=='select_boxes')||(x==src_x&&y==src_y)||(x==dest_x&&y==dest_y)) return ;
+                if(isMouseDownForMovingIcons){
+                    if(mouseDown_src){
+                        set_src(x,y);
+                    }
+                    else{
+                        set_dest(x,y);
+                    }
+                    return ;
+                }
+                if(isMouseDown==0||(x==src_x&&y==src_y)||(x==dest_x&&y==dest_y)) return ;
 
                 if(this.classList.contains('checked')) {
                     this.classList.remove('checked');
@@ -101,15 +97,6 @@ function addingEventListeners(){
                 }
             })
 
-            box.addEventListener('click',function(){
-                let [x,y]=this.id.split('-');
-                if(controlState=="change_src"){
-                    set_src(x,y);
-                }
-                else if(controlState=="change_dest"){
-                    set_dest(x,y);
-                }
-            })
             
         }
     )
@@ -117,8 +104,10 @@ function addingEventListeners(){
     grid_table.addEventListener('mousedown',function(){
         isMouseDown=1;
     })
-    grid_table.addEventListener('mouseup',function(){
+    document.addEventListener('mouseup',function(){
         isMouseDown=0;
+        isMouseDownForMovingIcons=0;
+        mouseDown_src=0;
     })
 
 
@@ -126,4 +115,4 @@ function addingEventListeners(){
 
 addingEventListeners();
 set_src(5,10);
-set_dest(10,15);
+set_dest(5,14);
